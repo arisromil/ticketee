46e7e2f227a23edc268379c7590199417392f630
arisromil <arisromil@yahoo.com>
2012-01-10 22:49:57 -0700

    Add validation to ensure names are specified when creating projects

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '4953103631bdec28c3bf286f27f436af92d129c3537829848b750f132d6b03e7f1ad3983b674878a1fc8da83b128dd808a2ab430b709e307544709831436ae70'
